4abacdc14791cc647fa590e921bb5de0f6e121c6
Ilya frontend <zaxarov.ilya.00@inbox.ru>
2024-05-01 14:31:00 +0500

    01.05 commit

--- server.js
+++ server.js
@@ -13,3 +13,31 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server start on port ${port}`);
 });
+
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+const storageFile = path.join(__dirname, "/storage/data.json");
+
+const dataFromClient = fs.existsSync(storageFile)
+  ? JSON.parse(fs.readFileSync(storageFile))
+  : console.log("error parse");
+
+app.get("/messages", (req, res) => {
+  res.send(dataFromClient);
+});
+
+app.post("/users/add", (req, res) => {
+  const msg = req.body;
+  // users.push({
+  //   id,
+  //   ...user,
+  // });
+
+  fs.writeFileSync(storageFile, JSON.stringify(msg));
+
+  res.send({
+    succes: true,
+    message: "Message added!",
+  });
+});
